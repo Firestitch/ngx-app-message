bb70c8d7d64c368f21152032e03e8b28428b8dbd
Anatoly Thernytskyi <mendor1990@gmail.com>
2025-04-01 11:11:15 +0300

    empty body error

--- src/app/modules/message-queues/pipes/truncate.pipe.ts
+++ src/app/modules/message-queues/pipes/truncate.pipe.ts
@@ -1,18 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+
 @Pipe({
-  name: 'truncate'
+  name: 'truncate',
 })
 export class TruncatePipe implements PipeTransform {
-transform(value: string, limit = 25, completeWords = false, ellipsis = '...') {
+  transform(value: string, limit = 25, completeWords = false, ellipsis = '...') {
+    value = value || '';
 
-  if (value.length < limit) {
-    return `${value.substr(0, limit)}`;
+    if (value.length < limit) {
+      return `${value.substr(0, limit)}`;
+    }
+
+    if (completeWords) {
+      limit = value.substr(0, limit).lastIndexOf(' ');
+    }
+
+    return `${value.substr(0, limit)}${ellipsis}`;
   }
-
-   if (completeWords) {
-     limit = value.substr(0, limit).lastIndexOf(' ');
-   }
-   return `${value.substr(0, limit)}${ellipsis}`;
-}
 }
