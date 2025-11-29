3063c6128120dd9090e4f2fed193e4ef8d1840b3
Raymond Gigliotti <ray@firestitch.com>
2025-11-29 04:29:17 -0500

    Refactor template component HTML for improved readability and consistency; update TypeScript imports and formatting.

--- src/app/modules/message-templates/components/template/template.component.ts
+++ src/app/modules/message-templates/components/template/template.component.ts
@@ -1,34 +1,47 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialog, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
+import { CdkScrollable } from '@angular/cdk/scrolling';
+import { MatButton } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatTab, MatTabContent, MatTabGroup } from '@angular/material/tabs';
 
+import { FsDialogModule } from '@firestitch/dialog';
+import { FsFormModule, SubmitEvent } from '@firestitch/form';
+import { FsLabelModule } from '@firestitch/label';
 import { FsMessage } from '@firestitch/message';
+import { FsSkeletonModule } from '@firestitch/skeleton';
 import { FsTextEditorConfig, FsTextEditorModule } from '@firestitch/text-editor';
-import { SubmitEvent, FsFormModule } from '@firestitch/form';
 
 import { tap } from 'rxjs/operators';
 
 import { PreviewComponent } from '../../../../modules/message-preview/components';
 import { FS_APP_MESSAGE_CONFIG } from '../../../app-message/injectors';
 import { FsAppMessageConfig } from '../../../app-message/interfaces';
-import { FsSkeletonModule } from '@firestitch/skeleton';
-import { FormsModule } from '@angular/forms';
-import { FsDialogModule } from '@firestitch/dialog';
-import { CdkScrollable } from '@angular/cdk/scrolling';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { MatTabGroup, MatTab, MatTabContent } from '@angular/material/tabs';
-import { FsLabelModule } from '@firestitch/label';
-import { MatButton } from '@angular/material/button';
 
 
 @Component({
-    templateUrl: './template.component.html',
-    styleUrls: ['./template.component.scss'],
-    standalone: true,
-    imports: [FsSkeletonModule, FormsModule, FsFormModule, FsDialogModule, MatDialogTitle, CdkScrollable, MatDialogContent, MatFormField, MatLabel, MatInput, MatTabGroup, MatTab, MatTabContent, FsLabelModule, FsTextEditorModule, MatDialogActions, MatButton, MatDialogClose]
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.scss'],
+  standalone: true,
+  imports: [FsSkeletonModule, FormsModule, FsFormModule, FsDialogModule, MatDialogTitle, CdkScrollable, MatDialogContent, MatFormField, MatLabel, MatInput, MatTabGroup, MatTab, MatTabContent, FsLabelModule, FsTextEditorModule, MatDialogActions, MatButton, MatDialogClose],
 })
 export class TemplateComponent implements OnInit {
+
+  public messageTemplate;    
+  public htmlEditorconfig: FsTextEditorConfig = {
+    language: 'html',
+    insertSpaces: true,
+    tabSize: 2,
+  };
+  public cssEditorconfig: FsTextEditorConfig = {
+    language: 'scss',
+    insertSpaces: true,
+    tabSize: 2,
+  };
+  
   private _data = inject(MAT_DIALOG_DATA);
   private _config = inject<FsAppMessageConfig>(FS_APP_MESSAGE_CONFIG);
   private _dialogRef = inject<MatDialogRef<TemplateComponent>>(MatDialogRef);
@@ -36,25 +49,12 @@ export class TemplateComponent implements OnInit {
   private _dialog = inject(MatDialog);
 
 
-  public messageTemplate;
-  public htmlEditorconfig: FsTextEditorConfig = {
-    language: 'html',
-    insertSpaces: true,
-    tabSize: 2,
-  };
-
-  public cssEditorconfig: FsTextEditorConfig = {
-    language: 'scss',
-    insertSpaces: true,
-    tabSize: 2,
-  };
-
   public ngOnInit(): void {
     if (this._data.messageTemplate.id) {
       this._config.loadMessageTemplate(this._data.messageTemplate)
-      .subscribe(messageTemplate => {
-        this.messageTemplate = messageTemplate;
-      });
+        .subscribe((messageTemplate) => {
+          this.messageTemplate = messageTemplate;
+        });
     } else {
       this.messageTemplate = {};
     }
@@ -78,7 +78,7 @@ export class TemplateComponent implements OnInit {
           if (event.submitter === 'save-close') {
             this._dialogRef.close(messageTemplate);
           }
-        })
+        }),
       );
-  }
+  };
 }
